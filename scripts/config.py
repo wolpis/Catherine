8776706b267ee76895379463a1645fc02b35b56d
VoidAsMad <wolpis_carter@jeonjeok.xyz>
2022-12-25 00:21:42 +0900

    feat(help) : feat help command

--- scripts/config.py
+++ scripts/config.py
@@ -4,11 +4,11 @@ from discord import Intents
 class CatherineConfig:
 
     token: str = (
-        "token"
+        "MTA1MjI2NDAzMDEzMzMwNTM2NA.GYRH6M.bBNvLoKbjsL2BLWcyJ6Q-D8NI6DO7tNe9gBd84"
     )
     guild_id: int = 1052256039455707176
     prefix: str = "?"
-    public: bool = False
+    public: bool = True
 
     def intents() -> Intents:
         intents = Intents.default()
